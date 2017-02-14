c314ffb78dd38b2060eb9600bdcfb012995b8482
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-02-14 16:51:55 +0100

    MonetDB: Cleanup create and drop table scripts
    Rename MonetDB scripts.sql to create-tables.sql.
    Add drop-tables.sql which removes all tables from create-tables.sql.
    This can be useful for jobs that need to start from scratch.

---
+++ Scripts/drop-tables.sql
@@ -0,0 +1,41 @@
+DROP TABLE "gros"."issue";
+
+DROP TABLE "gros"."issuetype";
+
+DROP TABLE "gros"."status";
+
+DROP TABLE "gros"."resolution";
+
+DROP TABLE "gros"."developer";
+
+DROP TABLE "gros"."fixversion";
+
+DROP TABLE "gros"."priority";
+
+DROP TABLE "gros"."relationshiptype";
+
+DROP TABLE "gros"."issuelink";
+
+DROP TABLE "gros"."subtask";
+
+DROP TABLE "gros"."ready_status";
+
+DROP TABLE "gros"."metric";
+
+DROP TABLE "gros"."metric_value";
+
+DROP TABLE "gros"."metric_version";
+
+DROP TABLE "gros"."metric_target";
+
+DROP TABLE "gros"."sprint";
+
+DROP TABLE "gros"."project";
+
+DROP TABLE "gros"."commits";
+
+DROP TABLE "gros"."comment";
+
+DROP TABLE "gros"."git_developer";
+
+DROP TABLE "gros"."git_repo";
