63ab9a0f39fb7c09819b9cbd02cf4b637d552f96
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-05-02 16:44:23 +0200

    Database dump: Correct format and commands
    - Database dumper: Use correct escape character
    - Import tables: Clean up MonetDB mclient commands, support passing more
      arguments, handle more errors

--- Scripts/import_tables.sh
+++ Scripts/import_tables.sh
@@ -1,34 +1,39 @@
 #!/bin/bash -e
 
 if [ -z $1 ] || [ "$1" = "--help" ] || [ -z $2 ]; then
-	echo "$0 <host> <dump directory>"
+	echo "$0 <host> <dump directory> [arguments]"
 	exit
 fi
 
 HOST=$1
-DIRECTORY=$2
+shift
+DIRECTORY=$1
+shift
+ARGUMENTS=$*
+
+ERROR_TEXT="syntax error|current transaction is aborted"
 
 function import() {
 	local table=$1
-	local error_text="current transaction is aborted"
+	local error_text="syntax error"
 
 	if [ ! -f "$DIRECTORY/$table.csv.gz" ]; then
 		echo "Could not find $DIRECTORY/$table.csv.gz"
-		exit
+		exit 1
 	fi
 
-	if [ -f "$DIRECTORY/$file.csv.gz" ]; then
+	if [ -f "$DIRECTORY/$table.csv.gz" ]; then
 		echo "Importing $table from CSV"
-		if [ "$(gunzip $DIRECTORY/$table.csv.gz | mclient -a -d gros -h $HOST -s \"COPY INTO gros.$table FROM STDIN USING DELIMITERS ',', '\n', '\"'\" - 2>&1 | tee /dev/stderr | grep -m 1 $error_text)" ]; then
-			exit
+		if [ "$(gzcat $DIRECTORY/$table.csv.gz | mclient -d gros -h $HOST $ARGUMENTS -s "COPY INTO gros.$table FROM STDIN USING DELIMITERS ',', '\n', '\"' NULL AS ''" - 2>&1 | tee /dev/stderr | grep -m 1 -E "$ERROR_TEXT")" ]; then
+			exit 1
 		fi
 		return
 	fi
 }
 
 echo "Importing schema"
-if [ "$(mclient -a -d gros -h $HOST $DIRECTORY/schema.sql 2>&1 | tee /dev/stderr | grep -m 1 "current transaction is aborted")" ]; then
-	exit
+if [ "$(cat $DIRECTORY/schema.sql | mclient -d gros -h $HOST $ARGUMENTS 2>&1 | tee /dev/stderr | grep -m 1 -E "$ERROR_TEXT")" ]; then
+	exit 1
 fi
 
 LINES=`cat create-tables.sql | grep '^CREATE TABLE' | \
