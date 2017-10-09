19a01ba97019016157320b9a562051211022cffd
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-10-09 14:08:12 +0200

    Schema validation: Check references
    - Unbreak table end matching again
    - Check whether references are valid between documentation parts:
      existence and types

--- Scripts/validate_schema.py
+++ Scripts/validate_schema.py
@@ -62,11 +62,12 @@ def test_line(line, current_tokens, data, parent_data=None):
             if 'line' in token:
                 test_line(line, token['line'], data, parent_data[name][groups[0]])
             if 'within' in token:
+                old_current_tokens = current_tokens.copy()
                 current_tokens = token['within']
                 if 'end' in token:
                     current_tokens['__end'] = {
                         'pattern': token['end'],
-                        'old': current_tokens.copy()
+                        'old': old_current_tokens
                     }
 
                 return current_tokens, parent_data[name][groups[0]]
@@ -87,7 +88,7 @@ def parse(tokens, line_iterator):
         current_tokens, parent_data = \
             test_line(line, current_tokens, data, parent_data)
 
-    logging.info('%r', data)
+    logging.debug('%r', data)
     return data
 
 def parse_schema(path):
@@ -195,6 +196,31 @@ def check_equal(one, two, key, extra=''):
 
     return True
 
+def check_reference(documentation, doc_field, field_text):
+    if 'reference' in doc_field:
+        ref_table_name, ref_field_name = doc_field['reference']
+        if ref_table_name not in documentation['table']:
+            logging.info('%r', documentation['table'].keys())
+            logging.warning('Invalid table reference %s%s',
+                            ref_table_name, field_text)
+            return False
+
+        ref_fields = documentation['table'][ref_table_name]['field']
+        if ref_field_name not in ref_fields:
+            logging.warning('Invalid field reference %s.%s%s',
+                            ref_table_name, ref_field_name, field_text)
+            return False
+
+        ref_field = ref_fields[ref_field_name]
+        if 'type' in doc_field and 'type' in ref_field and \
+            doc_field['type'] != ref_field['type']:
+            logging.warning('Referenced field %s.%s with type %s does not match type %s%s',
+                            ref_table_name, ref_field_name, ref_field['type'],
+                            doc_field['type'], field_text)
+            return False
+
+    return True
+
 def main():
     """
     Main entry point.
@@ -210,12 +236,19 @@ def main():
     is_ok = check_missing(schema, documentation, 'table')
 
     for table_name, table in schema['table'].items():
+        if table_name not in documentation['table']:
+            continue
+
+        logging.info('Checking table %s', table_name)
         table_text = ' for table {}'.format(table_name)
         doc_table = documentation['table'][table_name]
         is_ok = check_equal(doc_table, table, 'primary_key_combined', table_text) and is_ok
         is_ok = check_missing(table, doc_table, 'field', table_text) and is_ok
 
         for field_name, field in table['field'].items():
+            if field_name not in doc_table['field']:
+                continue
+
             field_text = ' of field {} in table {}'.format(field_name, table_name)
             doc_field = doc_table['field'][field_name]
             if check_equal(field, doc_field, 'null', field_text):
@@ -229,6 +262,8 @@ def main():
                     logging.warning('Table %s does not have primary key %s',
                                     table_name, field_name)
                     is_ok = False
+
+            is_ok = check_reference(documentation, doc_field, field_text) and is_ok
 
     sys.exit(0 if is_ok else 1)
 
