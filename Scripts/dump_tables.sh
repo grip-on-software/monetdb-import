dd7f5635be6288fbd5f05638048de662435182c4
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-02-09 15:49:45 +0100

    Scripts: Improve dump and import scripts
    - Avoid problems with spaces in paths
    - Generate relative paths from the script location so that it can be run
      from any location
    - Dump: Dry run option
    - Dump: Check if host is consistent with the dumper configuation
    - Dump: Remove stale variables
    - Import: Add argument for database name (schema name remains the same)

--- Scripts/dump_tables.sh
+++ Scripts/dump_tables.sh
@@ -1,51 +1,68 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 if [ -z $1 ] || [ "$1" = "--help" ]; then
-	echo "$0 <host> [output directory] [dumper path]"
+	echo "$0 <host|--help|--dry-run> [output directory] [dumper path] [dumper config]"
 	exit
 fi
 
-HOST=$1
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
-if [ ! -z $2 ]; then
-	OUTPUT_DIRECTORY=$2
+HOST="$1"
+
+if [ ! -z "$2" ]; then
+	OUTPUT_DIRECTORY="$2"
 fi
-if [ ! -z $3 ]; then
-	DUMPER_PATH=$2
+if [ ! -z "$3" ]; then
+	DUMPER_PATH="$3"
+fi
+if [ ! -z "$4" ]; then
+	DUMPER_CONFIG="$4"
 fi
 
-if [ -z $OUTPUT_DIRECTORY ]; then
+if [ -z "$OUTPUT_DIRECTORY" ]; then
 	OUTPUT_DIRECTORY=`pwd`
 fi
-if [ -z $DUMPER_PATH ]; then
-	DUMPER_PATH=~/monetdb-dumper/dist/databasedumper.jar
+if [ -z "$DUMPER_PATH" ]; then
+	DUMPER_PATH="$SCRIPT_DIR/../../monetdb-dumper/dist/databasedumper.jar"
+	DUMPER_CONFIG="$SCRIPT_DIR/../../monetdb-dumper/nbproject/private/config.properties"
 fi
-
-echo $OUTPUT_DIRECTORY
 
 TIMESTAMP=`date +%Y-%m-%d`
 DUMP_DIRECTORY="$OUTPUT_DIRECTORY/gros-$TIMESTAMP"
 
-if [ -d $DUMP_DIRECTORY ]; then
-	rm -rf $DUMP_DIRECTORY
+echo "HOST=$HOST"
+echo "SCRIPT_DIR=$SCRIPT_DIR"
+echo "DUMPER_PATH=$DUMPER_PATH"
+echo "DUMP_DIRECTORY=$DUMP_DIRECTORY"
+
+if [ "$HOST" = "--dry-run" ]; then
+	echo "DRY RUN"
+else
+	if [ ! -z "$DUMPER_CONFIG" ]; then
+		if [ ! $(grep databasedumper.url "$DUMPER_CONFIG" | grep -- "$HOST") ]; then
+			echo "Make sure the database dumper is configured for the host."
+			exit 1
+		fi
+	fi
+
+	if [ -d "$DUMP_DIRECTORY" ]; then
+		rm -rf "$DUMP_DIRECTORY"
+	fi
+	mkdir "$DUMP_DIRECTORY"
 fi
-mkdir $DUMP_DIRECTORY
 
-LARGE_TABLES="issue"
-LARGE_DB_DIRECTORY="/home/monetdb"
-
-if [ $HOST = "localhost" ]; then
-	LARGE_DB_DIRECTORY="$DUMP_DIRECTORY"
-fi
-
-LINES=`cat create-tables.sql | grep '^CREATE TABLE' | \
+LINES=`cat "$SCRIPT_DIR/create-tables.sql" | grep '^CREATE TABLE' | \
 	sed -e 's/CREATE TABLE ".*"\."\(.*\)" (/\1/'`
 
 for line in $LINES
 do
 	echo "Dumping $line"
-	java -jar $DUMPER_PATH $line $DUMP_DIRECTORY/$line.csv.gz
+	if [ "$HOST" != "--dry-run" ]; then
+		java -jar $DUMPER_PATH $line "$DUMP_DIRECTORY/$line.csv.gz"
+	fi
 done
 
 echo "Dumping schema"
-msqldump -d gros -h $HOST -D > $DUMP_DIRECTORY/schema.sql
+if [ "$HOST" != "--dry-run" ]; then
+	msqldump -d gros -h $HOST -D > "$DUMP_DIRECTORY/schema.sql"
+fi
--- Scripts/import_tables.sh
+++ Scripts/import_tables.sh
@@ -1,15 +1,23 @@
 #!/bin/bash -e
 
 if [ -z $1 ] || [ "$1" = "--help" ] || [ -z $2 ]; then
-	echo "$0 <host> <dump directory> [arguments]"
+	echo "$0 <host> <dump directory> [dbname] [arguments]"
 	exit
 fi
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 HOST=$1
 shift
 DIRECTORY=$1
 shift
-ARGUMENTS=$*
+if [ ! -z $1 ]; then
+	DATABASE=$1
+	shift
+else
+	DATABASE="gros"
+fi
+ARGUMENTS=$@
 
 ERROR_TEXT="syntax error|current transaction is aborted"
 
@@ -24,7 +32,7 @@ function import() {
 
 	if [ -f "$DIRECTORY/$table.csv.gz" ]; then
 		echo "Importing $table from CSV"
-		if [ "$(gzcat $DIRECTORY/$table.csv.gz | mclient -d gros -h $HOST $ARGUMENTS -s "COPY INTO gros.$table FROM STDIN USING DELIMITERS ',', '\n', '\"' NULL AS ''" - 2>&1 | tee /dev/stderr | grep -m 1 -E "$ERROR_TEXT")" ]; then
+		if [ "$(gzcat "$DIRECTORY/$table.csv.gz" | mclient -d gros -h $HOST $ARGUMENTS -s "COPY INTO gros.$table FROM STDIN USING DELIMITERS ',', '\n', '\"' NULL AS ''" - 2>&1 | tee /dev/stderr | grep -m 1 -E "$ERROR_TEXT")" ]; then
 			exit 1
 		fi
 		return
@@ -32,11 +40,11 @@ function import() {
 }
 
 echo "Importing schema"
-if [ "$(cat $DIRECTORY/schema.sql | mclient -d gros -h $HOST $ARGUMENTS 2>&1 | tee /dev/stderr | grep -m 1 -E "$ERROR_TEXT")" ]; then
+if [ "$(cat "$DIRECTORY/schema.sql" | mclient -d $DATABASE -h $HOST $ARGUMENTS 2>&1 | tee /dev/stderr | grep -m 1 -E "$ERROR_TEXT")" ]; then
 	exit 1
 fi
 
-LINES=`cat create-tables.sql | grep '^CREATE TABLE' | \
+LINES=`cat "$SCRIPT_DIR/create-tables.sql" | grep '^CREATE TABLE' | \
 	sed -e 's/CREATE TABLE ".*"\."\(.*\)" (/\1/'`
 
 for line in $LINES
