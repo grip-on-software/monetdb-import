03f32c275af3e67635bd942a50023ecede12c23d
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-02-12 11:24:54 +0100

    Dump tables: Improvements
    - Replace positional argument parsing with long and short options
    - Option to pass define properties to the Java dumper
    - List of large tables which are dumped with the MonetDB dumper rather
      than the Java dumper.

--- Scripts/dump_tables.sh
+++ Scripts/dump_tables.sh
@@ -1,14 +1,91 @@
 #!/bin/bash -e
 
-if [ -z $1 ] || [ "$1" = "--help" ]; then
-	echo "$0 <host|--help|--dry-run> [output directory] [skip pattern]"
-	echo "   [dumper path] [dumper config]"
+function do_help() {
+	echo "$0 <-h host|--help|--dry-run> [-o|--output directory]"
+	echo "   [-s|--skip pattern] [-p|--dumper path] [-c|--config properies]"
+	echo "   [-d|--defines ...]"
+	echo ""
+	echo "Perform a (partial) database dump of the GROS database."
+	echo "Each table is written into a separate file in a timestamped output"
+	echo "directory; depending on whether it is considered a large table,"
+	echo "the file is named <table>.sql.gz or <table>.csv.gz. Additionally,"
+	echo "the database schema is dumped to schema.sql."
 	exit
+}
+
+if [ -z $1 ]; then
+	do_help
 fi
 
-SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+# Check if an element is in a space-separated list.
+# Returns 0 if the element is in the list, and 1 otherwise.
+function is_in_list() {
+	local element=$1
+	shift
+	local list=$*
 
-HOST="$1"
+	set +e
+	echo $list | grep -w -q "\b$element\b"
+	local status=$?
+	set -e
+
+	return $status
+}
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+SKIP_PATTERN="^$"
+
+while [[ $# -gt 0 ]]; do
+	key="$1"
+	case $key in
+		-h)
+			if [[ $# -lt 2 || "$2" == "-*" ]]; then
+				do_help
+			fi
+
+			HOST="$2"
+			shift
+			shift
+			;;
+		--help)
+			do_help
+			;;
+		--dry-run)
+			HOST="$1"
+			shift
+			;;
+		-o|--output)
+			OUTPUT_DIRECTORY="$2"
+			shift
+			shift
+			;;
+		-s|--skip)
+			SKIP_PATTERN="$2"
+			shift
+			shift
+			;;
+		-p|--dumper)
+			DUMPER_PATH="$2"
+			shift
+			shift
+			;;
+		-c|--config)
+			DUMPER_CONFIG="$2"
+			shift
+			shift
+			;;
+		-d|--defines)
+			shift
+			while [[ $# -gt 0 && $1 != "-*" ]]; do
+				DEFINES="$DEFINES -D$1"
+				shift
+			done
+			;;
+		*)
+			do_help
+			;;
+	esac
+done
 
 if [ ! -z "$2" ]; then
 	OUTPUT_DIRECTORY="$2"
@@ -33,11 +110,13 @@ fi
 
 TIMESTAMP=`date +%Y-%m-%d`
 DUMP_DIRECTORY="$OUTPUT_DIRECTORY/gros-$TIMESTAMP"
+large_tables=$(cat "$SCRIPT_DIR/large_tables.txt")
 
 echo "HOST=$HOST"
 echo "SCRIPT_DIR=$SCRIPT_DIR"
 echo "DUMPER_PATH=$DUMPER_PATH"
 echo "DUMP_DIRECTORY=$DUMP_DIRECTORY"
+echo "DEFINES=$DEFINES"
 
 if [ "$HOST" = "--dry-run" ]; then
 	echo "DRY RUN"
@@ -62,7 +141,11 @@ for line in $LINES
 do
 	echo "Dumping $line"
 	if [ "$HOST" != "--dry-run" ]; then
-		java -jar $DUMPER_PATH $line "$DUMP_DIRECTORY/$line.csv.gz"
+		if is_in_list $line $large_tables; then
+			msqldump -d gros -h $HOST -t "gros.$line" | gzip -c > "$DUMP_DIRECTORY/$line.sql.gz"
+		else
+			java $DEFINES -jar $DUMPER_PATH $line "$DUMP_DIRECTORY/$line.csv.gz"
+		fi
 	fi
 done
 
