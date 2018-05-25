cc01a40d4957347aceb24d93e7baff0a1b8670aa
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-05-25 10:20:54 +0200

    Import tables: Support SQL import
    - Check if a .sql.gz file exists and import from that instead of .csv.gz
    - Consistently use provided schema to determine which tables to import
    - Quote parameters

--- Scripts/import_tables.sh
+++ Scripts/import_tables.sh
@@ -5,13 +5,11 @@ if [ -z $1 ] || [ "$1" = "--help" ] || [ -z $2 ]; then
 	exit
 fi
 
-SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
-
 HOST=$1
 shift
 DIRECTORY=$1
 shift
-if [ ! -z $1 ]; then
+if [ ! -z "$1" ]; then
 	DATABASE=$1
 	shift
 else
@@ -25,26 +23,28 @@ function import() {
 	local table=$1
 	local error_text="syntax error"
 
-	if [ ! -f "$DIRECTORY/$table.csv.gz" ]; then
-		echo "Could not find $DIRECTORY/$table.csv.gz"
-		exit 1
-	fi
-
-	if [ -f "$DIRECTORY/$table.csv.gz" ]; then
-		echo "Importing $table from CSV"
-		if [ "$(gzcat "$DIRECTORY/$table.csv.gz" | mclient -d gros -h $HOST $ARGUMENTS -s "COPY INTO gros.$table FROM STDIN USING DELIMITERS ',', '\n', '\"' NULL AS ''" - 2>&1 | tee /dev/stderr | grep -m 1 -E "$ERROR_TEXT")" ]; then
+	if [ -f "$DIRECTORY/table.sql.gz" ]; then
+		echo "Importing $table from SQL"
+		if [ "$(gzcat "$DIRECTORY/$table.csv.gz" | mclient -d "$DATABASE" -h "$HOST" $ARGUMENTS -s "COPY INTO gros.$table FROM STDIN USING DELIMITERS ',', '\\n', '\"' NULL AS '\\007NUL\\007'" - 2>&1 | tee /dev/stderr | grep -m 1 -E "$ERROR_TEXT")" ]; then
 			exit 1
 		fi
-		return
+	elif [ -f "$DIRECTORY/$table.csv.gz" ]; then
+		echo "Importing $table from CSV"
+		if [ "$(gzcat "$DIRECTORY/$table.csv.gz" | mclient -d "$DATABASE" -h "$HOST" $ARGUMENTS -s "COPY INTO gros.$table FROM STDIN USING DELIMITERS ',', '\n', '\"' NULL AS '\\007NUL\\007'" - 2>&1 | tee /dev/stderr | grep -m 1 -E "$ERROR_TEXT")" ]; then
+			exit 1
+		fi
+	else
+		echo "Could not find $DIRECTORY/$table.sql.gz or $DIRECTORY/$table.csv.gz"
+		exit 1
 	fi
 }
 
 echo "Importing schema"
-if [ "$(cat "$DIRECTORY/schema.sql" | mclient -d $DATABASE -h $HOST $ARGUMENTS 2>&1 | tee /dev/stderr | grep -m 1 -E "$ERROR_TEXT")" ]; then
+if [ "$(cat "$DIRECTORY/schema.sql" | mclient -d "$DATABASE" -h "$HOST" $ARGUMENTS 2>&1 | tee /dev/stderr | grep -m 1 -E "$ERROR_TEXT")" ]; then
 	exit 1
 fi
 
-LINES=`cat "$SCRIPT_DIR/create-tables.sql" | grep '^CREATE TABLE' | \
+LINES=`cat "$DIRECTORY/schema.sql" | grep '^CREATE TABLE' | \
 	sed -e 's/CREATE TABLE ".*"\."\(.*\)" (/\1/'`
 
 for line in $LINES
