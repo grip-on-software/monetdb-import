33f9b22ffb88810096afe6bd9795809ce93825b0
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2023-05-16 15:34:04 +0200

    Include Copyright line for current maintainer

--- Scripts/validate_schema.py
+++ Scripts/validate_schema.py
@@ -3,6 +3,7 @@ Script to validate a documentation page against the database schema.
 
 Copyright 2017-2020 ICTU
 Copyright 2017-2022 Leiden University
+Copyright 2017-2023 Leon Helwerda
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
